08f6a03bc755d4cd9a865fb62da59e8c9b17c52a
phdbuilds <blnorris777@gmail.com>
2025-08-08 07:05:00 -0400

    Added transparency option to nvim and cleaned up unused powerlevel10k from zshrc

--- roles/neovim/files/init.lua
+++ roles/neovim/files/init.lua
@@ -776,6 +776,7 @@ require("lazy").setup({
 		config = function()
 			---@diagnostic disable-next-line: missing-fields
 			require("tokyonight").setup({
+				transparent = true,
 				styles = {
 					comments = { italic = false }, -- Disable italics in comments
 				},
@@ -892,9 +893,9 @@ require("lazy").setup({
 	--
 	--  Uncomment the following line and add your plugins to `lua/custom/plugins/*.lua` to get going.
 	-- { import = 'custom.plugins' },
-	
+
 	-- Import plugins from lua/plugins/ directory
-	{ import = 'plugins' },
+	{ import = "plugins" },
 	--
 	-- For additional information with loading, sourcing and examples see `:help lazy.nvim-🔌-plugin-spec`
 	-- Or use telescope!
