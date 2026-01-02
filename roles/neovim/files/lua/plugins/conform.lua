3f39abbf094e1b926f1a3c09e6fcaf25f1478642
phdbuilds <blnorris777@gmail.com>
2026-01-02 09:26:22 -0500

    Switched from goimports-revised to goimports

--- roles/neovim/files/lua/plugins/conform.lua
+++ roles/neovim/files/lua/plugins/conform.lua
@@ -2,7 +2,7 @@ return {
 	"stevearc/conform.nvim",
 	opts = {
 		formatters_by_ft = {
-			go = { "goimports-reviser", "gofumpt" },
+			go = { "goimports", "gofumpt" },
 		},
 		format_on_save = {
 			timeout_ms = 500,
